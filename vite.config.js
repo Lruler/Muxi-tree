5589006881b2d19b56a28ca283918a90dda8e613
lruler <1204312199@qq.com>
2022-03-19 17:26:17 +0800

    add server

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     port: '3000',
     open: false, //自动打开 
-    base: "./ ", //生产环境路径
+    base: "/", //生产环境路径
     proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
       // 正则表达式写法
       '^/api': {
